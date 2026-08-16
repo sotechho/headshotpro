0487c4291aa2c7cb66fad52ebf2219a1572248e7
marshaale <maxamedmuqtaaraxmed@gmail.com>
2026-08-16 16:39:59 +0300

    feat: add SMTP configuration options to the application

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,6 +1,32 @@
+import { parseBool } from "@/utils";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+type SMTPOptions = {
+  host: string;
+  port: number;
+  secure: boolean;
+  user: string;
+  password: string;
+  from: string;
+  logger: boolean;
+  debug: boolean;
+};
+
+const smtp: SMTPOptions = {
+  host: process.env.SMTP_HOST || "smtp.gmail.com",
+  port: Number(process.env.SMTP_PORT) || 587,
+  secure: parseBool(
+    process.env.SMTP_SECURE,
+    process.env.NODE_ENV === "production",
+  ),
+  user: process.env.SMTP_USER || "",
+  password: process.env.SMTP_PASSWORD || "",
+  from: process.env.EMAIL_FROM || "info@headshotpro.ai",
+  logger: process.env.NODE_ENV === "development",
+  debug: process.env.NODE_ENV === "development",
+};
 
 export const config = {
   env: process.env.NODE_ENV || "development",
@@ -19,7 +45,9 @@ export const config = {
     directory: "logs",
     files: {
       error: "error.log",
+      warn: "warn.log",
       combined: "combined.log",
     },
   },
+  smtp,
 };
