2fed233245043e33b4f0730e523915810493723d
Muqtaar Axmed Maxamed <143383104+MoDev40@users.noreply.github.com>
2026-08-20 13:58:01 +0300

    feat: add authentication middleware for token validation and user retrieval

---
+++ backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,61 @@
+import { User } from '@/models/User.model';
+import { tokenService } from '@/services/auth/token.service';
+import { NotFoundError, UnauthorizedError } from '@/utils/errors';
+import type { NextFunction, Request, Response } from 'express';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        userId: string;
+        email: string;
+        role: string;
+      };
+    }
+  }
+}
+
+export async function validate(
+  req: Request,
+  _res: Response,
+  next: NextFunction,
+) {
+  try {
+    let token = req.cookies.accessToken;
+
+    if (!token) {
+      const authorization = req.headers.authorization;
+      if (authorization && authorization.startsWith('Bearer ')) {
+        token = authorization.substring(7);
+      }
+    }
+
+    if (!token) {
+      throw new UnauthorizedError('access token is required');
+    }
+
+    const payload = tokenService.verifyAccessToken(token);
+
+    const user = await User.findById(payload.userId);
+
+    if (!user) {
+      throw new NotFoundError('user not exists');
+    }
+
+    if (!user.isActive) {
+      throw new UnauthorizedError(
+        'contact the support team your account is deactivated',
+      );
+    }
+
+    req.user = {
+      userId: user._id.toString(),
+      email: user.email,
+      role: user.role,
+    };
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
---
+++ backend/src/middlewares/index.ts
@@ -0,0 +1,3 @@
+export * from './error.middleware';
+export * from './validator.middleware';
+export * from './auth.middleware';
