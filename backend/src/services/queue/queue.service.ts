640fe611be8a943b73cfcc1ea7ce69e58b3a33de
Muqtaar Axmed Maxamed <143383104+MoDev40@users.noreply.github.com>
2026-09-05 08:49:03 +0300

    feat: add triggerGenerateHeadshots function and IGenerateHeadshotEventData interface

--- backend/src/services/queue/queue.service.ts
+++ backend/src/services/queue/queue.service.ts
@@ -3,10 +3,19 @@ import type { ISuccessfullPayment } from '@/types/payment.types';
 import { AppError } from '@/utils/errors';
 import logger from '@/utils/logger';
 import { addUserCredits } from './payment.queue';
+import type { HeadshotStyle } from '../headshot';
 
 export interface IAddCreditEventData extends ISuccessfullPayment {
   credits: number;
   userId: string;
+}
+
+export interface IGenerateHeadshotEventData {
+  userId:string;
+  headshotId:string;
+  photoUrl:string;
+  selectedStyles?:HeadshotStyle[]
+  customPrompt?:string
 }
 
 export async function triggerAddUserCredits(data: IAddCreditEventData) {
@@ -21,6 +30,21 @@ export async function triggerAddUserCredits(data: IAddCreditEventData) {
       500,
       'INNGEST_TRIGGER',
       'Failed to trigger add user credits ',
+    );
+  }
+}
+
+export async function triggerGenerateHeadshots(data:IGenerateHeadshotEventData){
+  try {
+    await inngest.send({
+      name:"headshot/generate-user-headshots",
+      data
+    })
+  } catch (error) {
+    throw new AppError(
+      500,
+      'INNGEST_TRIGGER',
+      'Failed to trigger generate headshot ',
     );
   }
 }
