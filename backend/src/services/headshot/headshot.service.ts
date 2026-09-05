e02f924c32a816329e1270c121b536552ca5aa8c
Muqtaar Axmed Maxamed <143383104+MoDev40@users.noreply.github.com>
2026-09-05 09:06:03 +0300

    feat: add generateHeadshot method with user validation, credit check and deduct, and event triggering

--- backend/src/services/headshot/headshot.service.ts
+++ backend/src/services/headshot/headshot.service.ts
@@ -1,5 +1,20 @@
 import { HEADSHOT_STYLES } from '@/constants';
+import { User } from '@/models';
+import { Headshot, type IHeadshot } from '@/models/Headshot.modal';
+import { s3Service } from '@/services/s3';
+import { InsufficientCreditError, NotFoundError } from '@/utils/errors';
+import logger from '@/utils/logger';
+import { triggerGenerateHeadshots } from '../queue';
+
 export type HeadshotStyle = keyof typeof HEADSHOT_STYLES;
+
+interface GenerateHeadshotParams {
+  userId: string;
+  selectedStyles?: string[];
+  customPrompt?: string;
+  fileBuffer: Buffer;
+  fileExtension: string;
+}
 
 class HeadshotService {
   getAvailableStyles(): { name: string; key: string; description: string }[] {
@@ -10,6 +25,75 @@ class HeadshotService {
       }),
     );
     return styles;
+  }
+
+  async generateHeadshot(params: GenerateHeadshotParams): Promise<IHeadshot> {
+    const { userId, selectedStyles, customPrompt, fileBuffer, fileExtension } =
+      params;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      throw new NotFoundError('User not found');
+    }
+
+    logger.info(`User ${userId} found with ${user.credits} credits`);
+    const creditsNeeded =
+      (selectedStyles || []).length + (customPrompt ? 1 : 0);
+    if (user.credits < creditsNeeded) {
+      logger.info(
+        `User ${userId} has insufficient credits: ${user.credits} < ${creditsNeeded}`,
+      );
+      throw new InsufficientCreditError(
+        `You do not have enough credits to generate a headshot: you need ${creditsNeeded} credits`,
+      );
+    }
+
+    logger.info(`Deducting ${creditsNeeded} credits from user ${userId}`);
+    user.credits -= creditsNeeded;
+    await user.save();
+
+    // upload
+    logger.info(`Upload file to s3`, {
+      userId,
+      fileBuffer,
+    });
+    const { key, url } = await s3Service.uploadOriginalFile(
+      userId,
+      fileBuffer,
+      fileExtension,
+    );
+
+    const oneDay = 24 * 60 * 60;
+    const signedUrl = await s3Service.getSignedUrl(key, oneDay);
+    logger.info('Generated signedUrl', {
+      userId,
+      signedUrl,
+      expiresIn: oneDay,
+    });
+
+    logger.info('Record headshot', {
+      userId,
+      key,
+      url,
+    });
+    const headshot = await Headshot.create({
+      user: user._id,
+      originalPhotoKey: key,
+      originalPhotoUrl: url,
+      selectedStyles,
+      customPrompt,
+      processingStartedAt: new Date().toISOString(),
+    });
+    logger.info('Triggering headshot generation event queue');
+
+    await triggerGenerateHeadshots({
+      headshotId: headshot._id.toString(),
+      userId,
+      photoUrl: signedUrl,
+      selectedStyles: selectedStyles as HeadshotStyle[],
+      customPrompt,
+    });
+    return headshot;
   }
 }
 
