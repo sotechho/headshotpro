447003fc3ea09abd5d654cf00b3ebc79c483a70d
Muqtaar Axmed Maxamed <143383104+MoDev40@users.noreply.github.com>
2026-09-10 16:37:13 +0300

    feat: add shadcn ui components

--- frontend/lib/hooks/index.ts
+++ frontend/lib/hooks/index.ts
@@ -1,3 +1,23 @@
 export * from './useAuth';
 export * from './useAdminActions';
 export * from './usePayment';
+
+import * as React from 'react';
+
+export function useMediaQuery(query: string): boolean {
+  const [value, setValue] = React.useState(false);
+
+  React.useEffect(() => {
+    function onChange(event: MediaQueryListEvent) {
+      setValue(event.matches);
+    }
+
+    const result = window.matchMedia(query);
+    setValue(result.matches);
+
+    result.addEventListener('change', onChange);
+    return () => result.removeEventListener('change', onChange);
+  }, [query]);
+
+  return value;
+}
