fc6114de527f2d54dbc4992cc10c1a8ecf3b9345
Muqtaar Axmed Maxamed <143383104+MoDev40@users.noreply.github.com>
2026-09-16 14:00:30 +0300

    fix: improve rate limit error message with remaining time and add logging.

--- backend/src/middlewares/rate-limit.middleware.ts
+++ backend/src/middlewares/rate-limit.middleware.ts
@@ -1,7 +1,8 @@
 import { rateLimitService } from '@/services/redis';
 import type { RateLimitConfig } from '@/types';
-import { TooManyRequestsError } from '@/utils/errors';
 import { getIdentifier } from '@/utils';
+import { TooManyRequestsError } from '@/utils/errors';
+import logger from '@/utils/logger';
 import type { NextFunction, Request, Response } from 'express';
 
 function formatDuration(seconds: number): string {
@@ -10,14 +11,17 @@ function formatDuration(seconds: number): string {
   }
 
   const minutes = Math.floor(seconds / 60);
-
   if (minutes < 60) {
     return `${minutes} minute${minutes !== 1 ? 's' : ''}`;
   }
 
   const hours = Math.floor(minutes / 60);
+  if (hours < 24) {
+    return `${hours} hour${hours !== 1 ? 's' : ''}`;
+  }
 
-  return `${hours} hour${hours !== 1 ? 's' : ''}`;
+  const days = Math.floor(hours / 24);
+  return `${days} day${days !== 1 ? 's' : ''}`;
 }
 
 export function rateLimitMiddleware(config: RateLimitConfig) {
@@ -31,14 +35,22 @@ export function rateLimitMiddleware(config: RateLimitConfig) {
           config.windowSeconds,
         );
 
+      logger.info('Reset at', {
+        resetAt,
+        key,
+      });
+
       res.setHeader('X-RateLimit-Limit', config.maxRequest);
       res.setHeader('X-RateLimit-Remaining', remaining);
       res.setHeader('X-RateLimit-Reset', resetAt.toISOString());
 
       if (!allowed) {
+        const resetAtSeconds = Math.floor(new Date(resetAt).getTime() / 1000);
+        const nowSeconds = Math.floor(Date.now() / 1000);
+        const secondsRemaining = Math.max(0, resetAtSeconds - nowSeconds);
         return next(
           new TooManyRequestsError(
-            `Too many requests rtry again after: ${formatDuration(config.windowSeconds)}`,
+            `Too many requests try again after: ${formatDuration(secondsRemaining)}`,
           ),
         );
       }
